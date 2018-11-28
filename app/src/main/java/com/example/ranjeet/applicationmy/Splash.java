fa4d5c7dd7070f7698f468e8fa15c6aa722fb859
karan Agarwal <30392398+Karanagarwal398@users.noreply.github.com>
2018-11-28 22:25:29 +0530

    Initial commit

---
+++ app/src/main/java/com/example/ranjeet/applicationmy/Splash.java
@@ -0,0 +1,28 @@
+package com.example.ranjeet.applicationmy;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                Intent i = new Intent(Splash.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 1000);
+    }
+
+
+}
